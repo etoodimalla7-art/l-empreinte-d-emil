8bc851ca47f0f534e0e73ffccb72e3c7e48f1c44
Etoo-dev <etoodimalla7@gmail.com>
2026-08-23 23:20:55 +0530

    Refactor import statement and add comment
    Updated import statement to use single quotes and added a comment.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,2 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+// OneSignal Web SDK v16 service worker. Keep this file at the site root.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
